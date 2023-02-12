396e637fc10b5d00f9191c55f77d389eeaeb85eb
Melih Karaberzat <90550594+melihkaraberzat@users.noreply.github.com>
2023-02-13 02:29:34 +0300

    Revert "Hasta ekleme özelliği"

--- src/main/java/com/LaboratuvarApp/HastalarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.LaboratuvarApp;
-
-import com.LaboratuvarApp.models.Hastalar;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HastalarRepository extends CrudRepository<Hastalar,Integer> {
-}
--- src/main/java/com/LaboratuvarApp/LaborantlarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.LaboratuvarApp;
-
-import com.LaboratuvarApp.models.Laborantlar;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LaborantlarRepository extends CrudRepository<Laborantlar,Integer> {
-}
