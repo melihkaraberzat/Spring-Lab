e1a71be1be315ce09ab1202cbf98f0d83238f925
Melih Karaberzat <melihkaraberzat@outlook.com>
2023-02-13 02:04:11 +0300

    Hasta ekleme özelliği

---
+++ src/main/java/com/LaboratuvarApp/LaborantlarRepository.java
@@ -0,0 +1,7 @@
+package com.LaboratuvarApp;
+
+import com.LaboratuvarApp.models.Laborantlar;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LaborantlarRepository extends CrudRepository<Laborantlar,Integer> {
+}
---
+++ src/main/java/com/LaboratuvarApp/LaboratuvarApplication.java
@@ -0,0 +1,15 @@
+package com.LaboratuvarApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class LaboratuvarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratuvarApplication.class, args);
+	}
+
+	}
+
